9061eaef45e5ab26a3233218dd22c94b608cae04
Ryan Godfrey <ryan_crothers@hotmail.com>
2020-03-28 16:19:21 -0400

    updated uploadBookData to generate random users + addresses

--- src/main/java/startup/uploadBookData.java
+++ src/main/java/startup/uploadBookData.java
@@ -4,6 +4,7 @@ package startup;/*
  */
 
 import com.google.gson.*;
+import backend.DatabaseQueries;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -55,6 +56,7 @@ public class uploadBookData {
         }catch (SQLException | IOException e) {
             e.printStackTrace();
         }
+
         new uploadBookData();
         System.out.println("Database has been updated");
     }
@@ -79,6 +81,11 @@ public class uploadBookData {
                     addPublishes(publishersObject, yearObject, isbnObject);
                 }
             });
+
+            // populate DB with users + addresses
+            for(int i = 0; i < 1000; i++)
+                addUsers();
+
         }catch (FileNotFoundException e){
             e.printStackTrace();
         }
@@ -212,6 +219,57 @@ public class uploadBookData {
                 }
             }
         });
+    }
+
+    private void addUsers(){
+        Random r = new Random();
+        String[] firstName = {"Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter", "Aiden", "Aidan", "Ayden", "Brad", "Bradley", "Connor", "Cathy", "Denver", "Daisere", "Evelynn", "Farah", "Gus", "Hale", "Iziac", "Jill", "Kyle", "Leah", "Monty", "Nathan", "Orion", "Penelope", "Quinn", "Ryan", "Stacey", "Tracy", "Umar", "Vern", "William", "Xia", "Yanny", "Zelda"};
+        String[] lastName = {"Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Paiser", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "VanZandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"};
+        String[] streetID = {"Blvd", "Rd", "St", "Ave", "Cres"};
+        String[] apartment = {"", "A", "B", "C", "D"};
+        String[] cities = {"Ottawa", "Toronto", "Montreal", "Calgary", "Vancouver", "Moscow", "Kelowna", "Windsor", "Haliburton", "Edmonton", "Winnipeg", "Saskatoon", "St. Johns", "Halifax"};
+        String[] provincesArr = {"AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT"};
+        String[] country = {"Canada", "USA", "Mexico", "Brazil", "Sweden", "Germany", "Russia", "China"};
+        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+        boolean[] trueFalse = {true, false};
+        // address deets
+        String defaultEmail = "email@default.ca";
+        String defaultStreetName = "Default ";
+        StringBuilder postalCode = new StringBuilder();
+        int streetNum = (int) (100L + r.nextFloat() * 900L); // 3 digit street num
+        boolean isBilling = trueFalse[r.nextInt(2)];
+        //generate postal code
+        for(int i = 0; i < 3; i++){
+            postalCode.append(letters[r.nextInt(letters.length)]).append(numbers[r.nextInt(numbers.length)]);
+        }
+        // user deets
+        String userFirstName = firstName[r.nextInt(firstName.length)];
+        String userLastName = lastName[r.nextInt(lastName.length)];
+        String username = userFirstName.substring(0,1) + userLastName.substring(0, 3); // username is 1st letter of first name + first 3 letters of last name
+        Integer salary = null;
+        int defaultSal = 30000;
+        if(trueFalse[r.nextInt(2)] && (username.contains("r") || username.contains("R"))) // decide if the user is an admin
+            salary = defaultSal;
+
+        try {
+            statement.executeUpdate("Insert into project.user values ('" + username + "', '" + username + "', '" + userFirstName + "', '" + userLastName + "', '" + defaultEmail + "');" +
+                    "insert into project.librarian values ('" + username + "'," + salary + ");");
+
+            DatabaseQueries.addAddress(Integer.toString(streetNum), defaultStreetName + streetID[r.nextInt(streetID.length)], apartment[r.nextInt(apartment.length)], cities[r.nextInt(cities.length)], provincesArr[r.nextInt(provincesArr.length)], country[r.nextInt(country.length)], postalCode.toString());
+            DatabaseQueries.addHasAdd(username, true, isBilling);
+
+            if(!isBilling) { // need another address
+                streetNum = (int) (100L + r.nextFloat() * 900L);
+                DatabaseQueries.addAddress(Integer.toString(streetNum), defaultStreetName + streetID[r.nextInt(streetID.length)], apartment[r.nextInt(apartment.length)], cities[r.nextInt(cities.length)], provincesArr[r.nextInt(provincesArr.length)], country[r.nextInt(country.length)], postalCode.toString());
+                DatabaseQueries.addHasAdd(username, false, true);
+            }
+
+        } catch (SQLException e) {
+            if(e.toString().equals("org.postgresql.util.PSQLException: ERROR: duplicate key value violates unique constraint \"user_pkey\"\n" +
+                    "  Detail: Key (user_name)=('" + username + "') already exists."))
+            e.printStackTrace();
+        }
     }
 
     private static void parseBookObject(JsonObject book){
